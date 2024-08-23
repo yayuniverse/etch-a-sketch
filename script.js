c1861fddb3798859d5ff853edb2a2f4d64abc155
yay <yaytrxsh@proton.me>
2024-08-23 02:59:34 +0100

    Added grid reset functionality

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const container = document.querySelector(".container");
 const containerWidth = parseInt(getComputedStyle(container).width, 10);
 
+function removeAllChildren(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
 function createSquare(number) {
+  removeAllChildren(container);
   for (let i = 1; i <= number * number; i++) {
     const square = document.createElement("div");
     square.style.width = `${containerWidth / number}px`;
     square.className = "square";
     container.appendChild(square);
   }
+  return "Done!";
 }
 
 createSquare(64);
-
-
