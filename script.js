a43316331ef607a1e39ade5be2f376318889c174
yay <yaytrxsh@proton.me>
2024-08-25 02:55:13 +0100

    Added Shade Mode

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ function createSquare(number) {
     const square = document.createElement("div");
     square.style.width = `${containerWidth / number}px`;
     square.className = "square";
+    // Initialize the data-opacity attribute
+    square.dataset.opacity = "0";
     container.appendChild(square);
   }
   return "Done!";
@@ -38,12 +40,20 @@ function randomColor() {
 }
 
 container.addEventListener("mouseover", function (event) {
-  if (event.target.matches(".square") && toggleColorMode.checked) {
-    event.target.style.backgroundColor = randomColor();
-    event.target.style.borderColor = event.target.style.backgroundColor;
-  } else if (event.target.matches(".square") && !toggleColorMode.checked) {
-    event.target.style.backgroundColor = "white";
-    event.target.style.borderColor = "white";
+  if (event.target.matches(".square")) {
+    if (toggleShadeMode.checked) {
+      let opacityCount = parseFloat(event.target.dataset.opacity);
+      opacityCount = Math.min(opacityCount + 0.1, 1);
+      event.target.style.backgroundColor = `rgba(255, 255, 255, ${opacityCount})`;
+      event.target.dataset.opacity = opacityCount.toString();
+    } else if (toggleColorMode.checked) {
+      event.target.style.backgroundColor = randomColor();
+      event.target.style.borderColor = event.target.style.backgroundColor;
+    } else {
+      event.target.style.backgroundColor = "white";
+      event.target.style.borderColor = "white";
+      event.target.dataset.opacity = "0";
+    }
   }
 });
 
@@ -55,4 +65,3 @@ form.addEventListener("submit", function (event) {
   event.preventDefault(); // Prevent the form from actually submitting
   createSquare(input.value);
 });
-
