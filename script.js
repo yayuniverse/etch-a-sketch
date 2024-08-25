645abb5c04b4528bef32bc83565ca99fd6af4d8f
yay <yaytrxsh@proton.me>
2024-08-25 02:07:55 +0100

    Added User Input and Color Mode Toggle

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const container = document.querySelector(".container");
 const containerWidth = parseInt(getComputedStyle(container).width, 10);
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const form = document.querySelector("form");
+const toggleColorMode = document.querySelector("#toggleColorMode");
+const toggleShadeMode = document.querySelector("#toggleShadeMode");
+
+createSquare(64);
 
 function removeAllChildren(parent) {
   while (parent.firstChild) {
@@ -18,13 +25,34 @@ function createSquare(number) {
   return "Done!";
 }
 
+function randomColor() {
+  return (
+    "rgb(" +
+    Math.floor(Math.random() * 256) +
+    "," +
+    Math.floor(Math.random() * 256) +
+    "," +
+    Math.floor(Math.random() * 256) +
+    ")"
+  );
+}
+
 container.addEventListener("mouseover", function (event) {
-    if (event.target.matches(".square")) {
-      //   console.log(typeof(event.target.style.opacity))
-      event.target.style.backgroundColor = "white";
-      event.target.style.borderColor = "white";
-    }
-  });
+  if (event.target.matches(".square") && toggleColorMode.checked) {
+    event.target.style.backgroundColor = randomColor();
+    event.target.style.borderColor = event.target.style.backgroundColor;
+  } else if (event.target.matches(".square") && !toggleColorMode.checked) {
+    event.target.style.backgroundColor = "white";
+    event.target.style.borderColor = "white";
+  }
+});
 
+button.addEventListener("click", () => {
+  createSquare(input.value);
+});
 
-createSquare(64);
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the form from actually submitting
+  createSquare(input.value);
+});
+
